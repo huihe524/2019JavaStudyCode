ada3aea20d20b935e546e3dcff9cd9319038e660
hxj0 <44400257+hxj0@users.noreply.github.com>
2020-08-14 04:13:40 +0800

    fix jdbc bug

--- huihe-groupOne-jdbc/huihe-test/src/Test.java
+++ huihe-groupOne-jdbc/huihe-test/src/Test.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class Test {
     public static void main(String[] args) throws Exception{
@@ -9,26 +10,29 @@ public class Test {
         //增
         String sql = "insert into group1 values(?,?,?)";
         Object[] objs = new Object[]{"20181101153", "李四", 15};
-        int count = JDBCUtils.executeUpdate(conn, psmt, sql, objs);
+        psmt = conn.prepareStatement(sql);
+        int count = JDBCUtils.executeUpdate(psmt, objs);
         if(count > 0) System.out.println("添加成功");
-        JDBCUtils.closeResource(null, psmt, conn);
-/*
+
         //删
         sql = "delete from group1 where sid=?";
         objs = new Object[]{"20181101153"};
-        count = JDBCUtils.executeUpdate(conn, psmt, sql, objs);
+        psmt = conn.prepareStatement(sql);
+        count = JDBCUtils.executeUpdate(psmt, objs);
         if(count > 0) System.out.println("删除成功");
 
         //改
         sql = "update group1 set sname=? where sid=?";
-        objs = new Object[]{"候鑫杰", "20181101152"};
-        count = JDBCUtils.executeUpdate(conn, psmt, sql, objs);
+        objs = new Object[]{"ww", "20181101152"};
+        psmt = conn.prepareStatement(sql);
+        count = JDBCUtils.executeUpdate(psmt, objs);
         if(count > 0) System.out.println("修改成功");
 
         //查
         sql = "select * from group1 where sage=?";
         objs = new Object[]{18};
-        ResultSet rs = JDBCUtils.executeQuery(conn, psmt, sql, objs);
+        psmt = conn.prepareStatement(sql);
+        ResultSet rs = JDBCUtils.executeQuery(psmt, objs);
         while(rs.next()){
             System.out.println(rs.getObject(1)+"-"+
                     rs.getObject(2)+"-"+
@@ -36,6 +40,6 @@ public class Test {
         }
 
         //最终释放资源
-        JDBCUtils.closeResource(rs, psmt, conn);*/
+        JDBCUtils.closeResource(rs, psmt, conn);
     }
 }
